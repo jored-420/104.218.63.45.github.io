e1ae4a9eab229f7e572fdc869a56d480c6be7f5a
jored-420 <54770929+jored-420@users.noreply.github.com>
2019-09-01 08:31:42 -0700

    Update grab.js

--- grab.js
+++ grab.js
@@ -1,13 +1,1 @@
-    function move()
-    {
-        var myObject, f;
-        myObject = new ActiveXObject("Scripting.FileSystemObject");
-        f =   myObject.GetFile("/#/ip.txt");
-        f.Move("D:\\");
-    }
-    //-->
-    </script>
-    Move the file ip.txt
-    <form name="myForm">
-    <input type="Button" vvalue="Move File" onClick='move()'>
-    </form>
+
