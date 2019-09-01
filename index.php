e496281f3609ea84629509b89561205059d8a433
jored-420 <54770929+jored-420@users.noreply.github.com>
2019-09-01 07:46:15 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<?php
-print ($_SERVER['REMOTE_ADDR']; //prints the IP on the page (Obviously)
-?>
-<?php
-$ip = $_SERVER['REMOTE_ADDR']; //sets the visitors IP ($_SERVER['REMOTE_ADDR']) to the variable "$ip"
-$handle = fopen('ipaddresses.txt', 'a+'); //opens ipaddresses.txt for reading and writing (MUST HAVE FILE NAMED ipaddresses.txt)
-fwrite($handle, "{$ip}\n");  //writes the IP address to ipaddresses.txt
-fclose($handle); //closes ipaddresses.txt for reading and writing
-?>
-<?php
 <html>
-<?php
-$file = "ipaddresses.txt"; //turns the text file into a variable
-$f=fopen($file, 'a+'); //opens up the text file for reading and writing
-fwrite($f,$_SERVER['REMOTE_ADDR']."\n"); //writes the IP address to ipaddresses.txt
-fclose($f); //closes ipaddresses.txt for reading and writing
-?>
+<a href="my.php">Not Found</a>
+<body>
+       
 <h1>File Not Found</h1>
 </body>
 </html> 
