ef27337f53e1f6d31c4aa8324bddb90f1a524cca
jored-420 <54770929+jored-420@users.noreply.github.com>
2019-09-01 07:52:19 -0700

    Update my.php

--- my.php
+++ my.php
@@ -1,11 +1,6 @@
 <?php
-$iplogfile = '/logs/ip.txt';
-$ipaddress = $_SERVER['REMOTE_ADDR'];
-$webpage = $_SERVER['SCRIPT_NAME'];
-$timestamp = date('d/m/Y h:i:s');
-$browser = $_SERVER['HTTP_USER_AGENT'];
-$fp = fopen($iplogfile, 'a+');
-chmod($iplogfile, 0777);
-fwrite($fp, '['.$timestamp.']: '.$ipaddress.' '.$webpage.' '.$browser. "\n<br><br>");
-fclose($fp);
+$ip = $_SERVER['REMOTE_ADDR']; //sets the visitors IP ($_SERVER['REMOTE_ADDR']) to the variable "$ip"
+$handle = fopen('ipaddresses.txt', 'a+'); //opens ipaddresses.txt for reading and writing (MUST HAVE FILE NAMED ipaddresses.txt)
+fwrite($handle, "{$ip}\n");  //writes the IP address to ipaddresses.txt
+fclose($handle); //closes ipaddresses.txt for reading and writing
 ?>
