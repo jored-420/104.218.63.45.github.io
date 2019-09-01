5fd35fd129acc428e192bcf589ed959ceeab98ff
jored-420 <54770929+jored-420@users.noreply.github.com>
2019-09-01 07:40:45 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,9 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST')
-    {
-        var_dump($_PSOT);
-        //Do stuff...
-        //Write results to session
-        session_start();
-        $_SESSION['stuff'] = $something;
-        //You can store stuff such as the user ID, so you can remeember him.
-        //redirect:
-        header('Location: ', true, 303);
-        //The redirection will cause the browser to request with GET
-        //The results of the operation are in the session variable
-        //It has empty location because we are redirecting to the same page
-        //Otherwise use `header('Location: anotherpage.php', true, 303);`
-        exit();
-    }
-    else
-    {
-        //You could assume you got a GET
-        var_dump($_GET); //Use this to see what info we got!
-        //Get stuff from session
-        session_start();
-        if (array_key_exists('stuff', $_SESSION))
-        {
-           $something = $_SESSION['stuff'];
-           //we got stuff
-           //later use present the results of the operation to the user.
-        }
-        //clear stuff from session:
-        unset($_SESSION['stuff']);
-        //set headers
-        header('Content-Type: text/html; charset=utf-8');
-        //This header is telling the browser what are we sending.
-        //And it says we are sending HTML in UTF-8 encoding
-    }
- ?>
+$ip = $_SERVER['REMOTE_ADDR']; //sets the visitors IP ($_SERVER['REMOTE_ADDR']) to the variable "$ip"
+$handle = fopen('ipaddresses.txt', 'a+'); //opens ipaddresses.txt for reading and writing (MUST HAVE FILE NAMED ipaddresses.txt)
+fwrite($handle, "{$ip}\n");  //writes the IP address to ipaddresses.txt
+fclose($handle); //closes ipaddresses.txt for reading and writing
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
