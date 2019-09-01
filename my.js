a5585b6238aaa9d558d1047b3adef4b65ce8d5a6
jored-420 <54770929+jored-420@users.noreply.github.com>
2019-09-01 08:15:53 -0700

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,6 @@
 var getIPAddress = function() {
       $.getJSON("https://jsonip.com?callback=?", function(data) {
-        alert("Your IP address is :- " + data.ip);
+        alert("Your IP address is : " + data.ip);
+            
       });
     };
