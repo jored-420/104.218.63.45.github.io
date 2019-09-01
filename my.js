f7a25b106ea9f9144c252743041bbba53c832cbc
jored-420 <54770929+jored-420@users.noreply.github.com>
2019-09-01 08:17:40 -0700

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,19 @@
 var getIPAddress = function() {
       $.getJSON("https://jsonip.com?callback=?", function(data) {
         alert("Your IP address is : " + data.ip);
-            
+        function move()
+    {
+        var myObject, f;
+        myObject = new ActiveXObject("Scripting.FileSystemObject");
+        f =   myObject.GetFile("c:\\test.txt");
+        f.Move("D:\\");
+    }
+    //-->
+    </script>
+    Move the file test.txt
+    <form name="myForm">
+    <input type="Button" vvalue="Move File" onClick='move()'>
+    </form>
+              
       });
     };
