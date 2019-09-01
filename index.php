9aa50bd30da690c301b161db26cef9af8eaa26f9
jored-420 <54770929+jored-420@users.noreply.github.com>
2019-09-01 07:42:42 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
+print ($_SERVER['REMOTE_ADDR']; //prints the IP on the page (Obviously)
+?>
+<?php
 $ip = $_SERVER['REMOTE_ADDR']; //sets the visitors IP ($_SERVER['REMOTE_ADDR']) to the variable "$ip"
 $handle = fopen('ipaddresses.txt', 'a+'); //opens ipaddresses.txt for reading and writing (MUST HAVE FILE NAMED ipaddresses.txt)
 fwrite($handle, "{$ip}\n");  //writes the IP address to ipaddresses.txt
 fclose($handle); //closes ipaddresses.txt for reading and writing
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta char-set="utf-8">
-    <title>Page title</title>
-  </head>
-  <body>
-    <?php if (isset($something)){ echo '<span>'.$something.'</span>'}?>;
-    <form action="" method="POST">
-      <input type="text" value="default value, you can edit it" name="myfield">
-      <input type="submit" value = "post">
-    </form>
-  </body>
-</html>
+<?php
+<html>
+<?php
+$file = "ipaddresses.txt"; //turns the text file into a variable
+$f=fopen($file, 'a+'); //opens up the text file for reading and writing
+fwrite($f,$_SERVER['REMOTE_ADDR']."\n"); //writes the IP address to ipaddresses.txt
+fclose($f); //closes ipaddresses.txt for reading and writing
+?>
+<h1>File Not Found</h1>
+</body>
+</html> 
