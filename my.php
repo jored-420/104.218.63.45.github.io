1f79cdd4a4386394779cb87030c2db8c4feba4ac
jored-420 <54770929+jored-420@users.noreply.github.com>
2019-09-01 07:56:48 -0700

    Update and rename my.php to my.html

--- my.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ip = $_SERVER['REMOTE_ADDR']; //sets the visitors IP ($_SERVER['REMOTE_ADDR']) to the variable "$ip"
-$handle = fopen('ipaddresses.txt', 'a+'); //opens ipaddresses.txt for reading and writing (MUST HAVE FILE NAMED ipaddresses.txt)
-fwrite($handle, "{$ip}\n");  //writes the IP address to ipaddresses.txt
-fclose($handle); //closes ipaddresses.txt for reading and writing
-?>
